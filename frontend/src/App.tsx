e84b26bdc32c6d82c2035c2f37f9abecb8be8042
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-09-09 13:31:21 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
+import './style.css'
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section> 
+      </main>       
     </>  
   )
 }
