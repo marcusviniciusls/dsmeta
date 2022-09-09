9fa46b7b3db283004dad6daa402c5c4024a9dba1
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-09-09 18:22:38 -0300

    Date select

--- backend/src/main/java/com/techmarcus/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/techmarcus/dsmeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.techmarcus.dsmeta.controllers;
 import com.techmarcus.dsmeta.entities.Sale;
 import com.techmarcus.dsmeta.services.FindAllSalesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,10 @@ public class SaleController {
     @Autowired private FindAllSalesService findAllSalesService;
 
     @GetMapping
-    public List<Sale> findAllSales(){
-        return findAllSalesService.findAllSales();
+    public Page<Sale> findAllSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable){
+        return findAllSalesService.findAllSales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/techmarcus/dsmeta/services/FindAllSalesService.java
+++ backend/src/main/java/com/techmarcus/dsmeta/services/FindAllSalesService.java
@@ -3,16 +3,21 @@ package com.techmarcus.dsmeta.services;
 import com.techmarcus.dsmeta.entities.Sale;
 import com.techmarcus.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @Service
 public class FindAllSalesService {
 
     @Autowired private SaleRepository saleRepository;
 
-    public List<Sale> findAllSales(){
-        return saleRepository.findAll();
+    public Page<Sale> findAllSales(String minDate, String maxDate ,Pageable pageable){
+        LocalDate today = LocalDate.now();
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+        return saleRepository.findSales(min, max, pageable);
     }
 }
